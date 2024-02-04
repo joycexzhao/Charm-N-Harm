c1a91aac7c147060a7f92d362922c181ec3eea20
OkayJaime <acostajea@gmail.com>
2024-02-04 15:05:06 -0600

    second try

--- Charm_N_Harm/Assets/Player.cs
+++ Charm_N_Harm/Assets/Player.cs
@@ -1,24 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement; // Include this namespace for scene management
 
 public class Player : MonoBehaviour
 {
-
     public float speed = 5f;
-    public float dash = 20f;
+    public float health = 100f;
+    public TextMeshProUGUI healthDisplay;
 
     private Rigidbody2D rb;
     private Vector2 move;
-    
+    private Coroutine damageCoroutine;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        UpdateHealthDisplay();
     }
 
-    // Update is called once per frame
     void Update()
     {
         move.x = Input.GetAxisRaw("Horizontal");
@@ -30,4 +31,42 @@ public class Player : MonoBehaviour
         rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && damageCoroutine == null)
+        {
+            damageCoroutine = StartCoroutine(DamageOverTime());
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && damageCoroutine != null)
+        {
+            StopCoroutine(damageCoroutine);
+            damageCoroutine = null;
+        }
+    }
+
+    private IEnumerator DamageOverTime()
+    {
+        while (true)
+        {
+            health -= 10f;
+            UpdateHealthDisplay();
+
+            if (health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload the current scene
+                yield break; // Exit the coroutine
+            }
+
+            yield return new WaitForSeconds(1f); // Wait for 1 second before applying damage again
+        }
+    }
+
+    void UpdateHealthDisplay()
+    {
+        healthDisplay.text = "Health: " + health;
+    }
 }
