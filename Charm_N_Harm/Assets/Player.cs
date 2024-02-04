977a8b16615c408093a26ccc424f5893a8fd5777
joycexzhao <joycexzhao@gmail.com>
2024-02-04 00:01:20 -0600

    simple movement & camera follow

---
+++ Charm_N_Harm/Assets/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+    }
+
+}
